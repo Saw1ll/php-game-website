c4489a90f0a0bba72464351112a840a9b473c39e
Sawill <williamssaw05@gmail.com>
2023-11-25 03:24:32 +0000

    dashboard more organised

--- website/comps/cards/cards/1stRow.php
+++ website/comps/cards/cards/1stRow.php
@@ -1,22 +1,17 @@
 <?php
-$cards1 = [
+$cards1 =
     [
-        'src' => 'images/hand-drawn-calculator-illustration_1375-5024.avif',
-        'text' => 'Here is my calculator app built in php',
-        'label' => 'Utility',
-        'path' => './projects/calculator/index.php'
-    ],
-    [
-        'src' => 'images/rng.png',
-        'text' => "Random Name Generator",
-        'label' => "Utility",
-        'path' => './projects/rng/index.php'
-    ],
-    [
-        'src' => 'images/427aba783806afea2075670ad99328be.jpg',
-        'text' => 'Guess The Number!',
-        'label' => 'Game',
-        'path' => './projects/guessTheNumber/guessTheNumber.php'
-    ]
-];
+        [
+            'src' => 'images/hand-drawn-calculator-illustration_1375-5024.avif',
+            'text' => 'PHP Calculator',
+            'label' => 'Utility',
+            'path' => './projects/calculator/index.php'
+        ],
+        [
+            'src' => 'images/rng.png',
+            'text' => "Random Name Generator",
+            'label' => "Utility",
+            'path' => './projects/rng/index.php'
+        ]
+    ];
 ?>
