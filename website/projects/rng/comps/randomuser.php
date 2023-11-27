3d4bf01cd1e0d03c846bff92f86411cde7e9d14e
Sawill <williamssaw05@gmail.com>
2023-11-27 05:33:07 +0000

    location of css updated, swapped buttons

--- website/projects/rng/comps/randomuser.php
+++ website/projects/rng/comps/randomuser.php
@@ -5,54 +5,59 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="../../../css/bootstrap.css">
+  <link rel="stylesheet" href="../../../css/styles.css">
   <title>Random Name Generator</title>
 </head>
 
 <body>
-  <?php
-  $nationality = isset($_POST['nationality']) ? $_POST['nationality'] : "";
-  $amount = isset($_POST['amount']) ? $_POST['amount'] : "";
-  $information = isset($_POST['information']) ? $_POST['information'] : "";
-  $nationalities = ['algerian', 'ag', 'american', 'us', 'australian', 'au', 'brazilian', 'br', 'canadian', 'ca', 'danish', 'da', 'dutch', 'nl', 'finnish', 'fi', 'french', 'fr', 'german', 'de', 'british', 'gb', 'irish', 'ie', 'new zealander', 'nz', 'spanish', 'es', 'swiss', 'ch', 'turkish', 'tr'];
-  if ($nationality != "no") {
-    if (
-      strlen($nationality) === 2 && in_array($nationality, $nationalities)
-    ) {
-      echo "<div class='heading'><p>Random names generated from the nationality: " . strtoupper($nationality) . "</p></div>";
-    } elseif (
-      strlen($nationality) != 2 && in_array($nationality, $nationalities)
-    ) {
-      echo "<div class='heading'><p>Random names generated from the nationality: " . ucfirst($nationality) . "</p></div>";
-    }
-  } else {
-    echo "<div class='heading'><p>Random names generated: </p></div>";
-  }
-
-  $user_response = file_get_contents("https://randomuser.me/api/?nat=$nationality&results=$amount&format=json");
-  $data_array = json_decode($user_response, true);
-  ?>
-  <div class="names_container">
-    <?php
-    for ($i = 0; $i < $amount; $i++) {
-      $full_name = $data_array['results'][$i]['name']['title'] . " " . $data_array['results'][$i]['name']['first'] . " " . $data_array['results'][$i]['name']['last'];
-      echo "<div class='names'><p>$full_name</p></div>";
-    }
-    ?>
+  <div class="dashboard">
+    <a href='../../index.php' class='btn btn-secondary'>Go back to dashboard!</a>
   </div>
-  <form method="post" action="information.php" required>
-    <select name='information'>
-      <option value='gender'>Gender</option>
-      <option value="address">Address</option>
-      <option value="email">Email</option>
-      <option value="dob">Date of Birth</option>
-      <option value="phone">Telephone Number</option>
-      <option value="cell">Cellphone Number</option>
-      <option value="nationality">Nationality</option>
-    </select>
-    <input type='submit' class='btn btn-primary'>
-  </form>
+  <div class="container">
+    <?php
+    $nationality = isset($_POST['nationality']) ? $_POST['nationality'] : "";
+    $amount = isset($_POST['amount']) ? $_POST['amount'] : "";
+    $information = isset($_POST['information']) ? $_POST['information'] : "";
+    $nationalities = ['algerian', 'ag', 'american', 'us', 'australian', 'au', 'brazilian', 'br', 'canadian', 'ca', 'danish', 'da', 'dutch', 'nl', 'finnish', 'fi', 'french', 'fr', 'german', 'de', 'british', 'gb', 'irish', 'ie', 'new zealander', 'nz', 'spanish', 'es', 'swiss', 'ch', 'turkish', 'tr'];
+    if ($nationality != "no") {
+      if (
+        strlen($nationality) === 2 && in_array($nationality, $nationalities)
+      ) {
+        echo "<div class='heading'><p>Random names generated from the nationality: " . strtoupper($nationality) . "</p></div>";
+      } elseif (
+        strlen($nationality) != 2 && in_array($nationality, $nationalities)
+      ) {
+        echo "<div class='heading'><p>Random names generated from the nationality: " . ucfirst($nationality) . "</p></div>";
+      }
+    } else {
+      echo "<div class='heading'><p>Random names generated: </p></div>";
+    }
+
+    $user_response = file_get_contents("https://randomuser.me/api/?nat=$nationality&results=$amount&format=json");
+    $data_array = json_decode($user_response, true);
+    ?>
+    <div class="names_container">
+      <?php
+      for ($i = 0; $i < $amount; $i++) {
+        $full_name = $data_array['results'][$i]['name']['title'] . " " . $data_array['results'][$i]['name']['first'] . " " . $data_array['results'][$i]['name']['last'];
+        echo "<div class='names'><p>$full_name</p></div>";
+      }
+      ?>
+    </div>
+    <form method="post" action="information.php" required>
+      <select name='information'>
+        <option value='gender'>Gender</option>
+        <option value="address">Address</option>
+        <option value="email">Email</option>
+        <option value="dob">Date of Birth</option>
+        <option value="phone">Telephone Number</option>
+        <option value="cell">Cellphone Number</option>
+        <option value="nationality">Nationality</option>
+      </select>
+      <input type='submit' class='btn btn-primary'>
+    </form>
+  </div>
 </body>
 
 </html>
--- website/projects/rng/index.php
+++ website/projects/rng/index.php
@@ -13,44 +13,46 @@
 
 <body>
     <div class="dashboard">
-        <a href='../../index.php' class='btn btn-primary'>Go back to dashboard!</a>
+        <a href='../../index.php' class='btn btn-secondary'>Go back to dashboard!</a>
     </div>
-    <div class="title">
-        <h2>Random Name Generator</h3>
-    </div>
-    <div class="intro">
-        <h4>Hello, welcome to the Random Name Generator</h4>
-        <h4>There will be a series of questions that you need to answer in order to generate your random names.</h4>
-        <h4>Let's get started,</h4>
-    </div>
-    <div class="questions">
-        <form method="post" action="./comps/randomuser.php">
-            <label for="amount">How many names would you like to generate? (Max 250)</label>
-            <input type="number" name='amount' min='1' max='250' id="amount" required>
-            <br><br>
-            <label for="nationality">Select a nationality or enter 'no' to generate without constraints</label>
-            <select name="nationality" id="nationality" required>
-                <option value="null">Select one</option>
-                <option value="ag">Algerian</option>
-                <option value="us">American</option>
-                <option value="au">Australian</option>
-                <option value="br">Brazillian</option>
-                <option value="ca">Canadian</option>
-                <option value="da">Danish</option>
-                <option value="nl">Dutch</option>
-                <option value="fi">Finnish</option>
-                <option value="fr">French</option>
-                <option value="de">German</option>
-                <option value="gb">British</option>
-                <option value="ie">Irish</option>
-                <option value="nz">New Zealander</option>
-                <option value="es">Spanish</option>
-                <option value="ch">Swiss</option>
-                <option value="tr">Turkish</option>
-            </select>
-            <br><br>
-            <button type="submit">Submit Query</button>
-        </form>
+    <div class="container">
+        <div class="title">
+            <h2>Random Name Generator</h3>
+        </div>
+        <div class="intro">
+            <h4>Hello, welcome to the Random Name Generator</h4>
+            <h4>There will be a series of questions that you need to answer in order to generate your random names.</h4>
+            <h4>Let's get started,</h4>
+        </div>
+        <div class="questions">
+            <form method="post" action="./comps/randomuser.php">
+                <label for="amount">How many names would you like to generate? (Max 250)</label>
+                <input type="number" name='amount' min='1' max='250' id="amount" required>
+                <br><br>
+                <label for="nationality">Select a nationality or enter 'no' to generate without constraints</label>
+                <select name="nationality" id="nationality" required>
+                    <option value="null">Select one</option>
+                    <option value="ag">Algerian</option>
+                    <option value="us">American</option>
+                    <option value="au">Australian</option>
+                    <option value="br">Brazillian</option>
+                    <option value="ca">Canadian</option>
+                    <option value="da">Danish</option>
+                    <option value="nl">Dutch</option>
+                    <option value="fi">Finnish</option>
+                    <option value="fr">French</option>
+                    <option value="de">German</option>
+                    <option value="gb">British</option>
+                    <option value="ie">Irish</option>
+                    <option value="nz">New Zealander</option>
+                    <option value="es">Spanish</option>
+                    <option value="ch">Swiss</option>
+                    <option value="tr">Turkish</option>
+                </select>
+                <br><br>
+                <button class='btn btn-primary' type="submit">Submit Query</button>
+            </form>
+        </div>
     </div>
 </body>
 
